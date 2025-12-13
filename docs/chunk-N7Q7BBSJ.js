22113822ca8c7a9a7fe54f7d046b7e84e695a266
Ali Mohammad Hayajneh <hayajneh030@gmail.com>
2025-12-13 20:21:03 +0300

    edit

---
+++ docs/chunk-N7Q7BBSJ.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-WWURBD5D.js";import{b as t}from"./chunk-35YHXOGJ.js";import{_a as e}from"./chunk-4Q5SRD4K.js";var u=class extends t{cartItemId;quantity=1;product=new r;keyAttributeValues=[];get total(){return this.quantity*(this.product.price??0)}constructor(){super()}};var s=class extends e{cartId};export{u as a,s as b};
