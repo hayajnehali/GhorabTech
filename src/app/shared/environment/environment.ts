3dbd0a13a12fe1a3f1ed04dbaf4dae988fde9039
Ali Mohammad Hayajneh <hayajneh030@gmail.com>
2026-06-06 03:16:36 +0300

    design

--- src/app/shared/environment/environment.ts
+++ src/app/shared/environment/environment.ts
@@ -11,7 +11,29 @@ export const environment = {
   token_KEY: 'token',
   language_KEY: 'language',
 };
-
+// export const environment = {
+//   assetsUrl:
+//     // window.location.hostname === 'localhost'
+//     //   ?
+//        'https://10.223.15.166:7253/assets/',
+//       // : 'https://rasoul.runasp.net/assets/',
+//   apiUrl:
+//     // window.location.hostname === 'localhost'
+//       // ?
+//        'https://10.223.15.166:7253/api/'
+//       // : 'https://rasoul.runasp.net/api/',
+//       ,
+//   CART_KEY: 'cartKey',
+//   token_KEY: 'token',
+//   language_KEY: 'language',
+// };
+// export const environment = {
+//   assetsUrl:  'http://localhost/assets/' ,
+//   apiUrl:   'http://localhost/api/',
+//   CART_KEY: 'cartKey',
+//   token_KEY: 'token',
+//   language_KEY: 'language',
+// };
 
 //View
 //View
