d2fd0a6d1f2adbc48eb6d15897381d3b74d4d87c
Ali Mohammad Hayajneh <hayajneh030@gmail.com>
2026-06-14 05:17:25 +0300

    bulid

--- docs/chunk-BPEX24FX.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./chunk-CXZLVIFW.js";import{b as t}from"./chunk-KU6GPCNL.js";import{Ja as e}from"./chunk-4G3CJFKR.js";var u=class extends t{cartItemId;quantity=1;product=new r;keyAttributeValues=[];get total(){return this.quantity*(this.product.price??0)}constructor(){super()}};var s=class extends e{cartId};export{u as a,s as b};
