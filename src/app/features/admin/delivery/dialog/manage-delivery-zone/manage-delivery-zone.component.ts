93ac299a78834f49ec6303047b4ce956f7b67240
Ali Mohammad Hayajneh <hayajneh030@gmail.com>
2026-06-17 03:26:18 +0300

    Add manage delivery zone dialog and redesign

---
+++ src/app/features/admin/delivery/dialog/manage-delivery-zone/manage-delivery-zone.component.ts
@@ -0,0 +1,50 @@
+import { Component, Inject } from '@angular/core';
+import { BaseManageComponent } from '@core/base/base-manage-component';
+import {
+  DeliveryZone,
+  DeliveryZoneFilter,
+  DeliveryZoneResult,
+} from '../../models/delivery-zone';
+import { DeliveryZoneService } from '../../services/delivery-zone.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-manage-delivery-zone',
+  standalone: false,
+  templateUrl: './manage-delivery-zone.component.html',
+  styleUrl: './manage-delivery-zone.component.scss',
+})
+export class ManageDeliveryZoneComponent extends BaseManageComponent<
+  DeliveryZone,
+  DeliveryZoneResult,
+  DeliveryZoneFilter
+> {
+  constructor(
+    service: DeliveryZoneService,
+    public dialogRef: MatDialogRef<ManageDeliveryZoneComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public data: { id?: string },
+  ) {
+    super(service, DeliveryZone);
+  }
+
+  override ngOnInit(): void {
+    let id: string | undefined = this.data.id;
+    if (id) {
+      this.isAdd = false;
+      this.getById(id);
+    } else {
+      this.isAdd = true;
+    }
+    this.onInitData();
+  }
+  close() {
+    this.dialogRef.close();
+  }
+  override onCreate() {
+    this.close();
+  }
+  override onEdit() {
+    this.close();
+  }
+}
