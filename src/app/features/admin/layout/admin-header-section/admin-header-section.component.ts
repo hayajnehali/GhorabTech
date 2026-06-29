9039e9548f6b7219084f67a791889efc2b28464c
Ali Mohammad Hayajneh <hayajneh030@gmail.com>
2026-06-29 19:28:28 +0300

    add delivery cost

--- src/app/features/admin/layout/admin-header-section/admin-header-section.component.ts
+++ src/app/features/admin/layout/admin-header-section/admin-header-section.component.ts
@@ -2,16 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { SOCIAL_LINKS, SocialLink } from '@core/model/social.config';
-import { TranslateModule, TranslateService } from '@ngx-translate/core'; 
-import { TopBarComponent } from "@shared/component/top-header/top-bar.component"; 
-import { LoginLogoutButtonComponent } from "@shared/component/login-logout-button/login-logout-button.component";
-import { LanguageButtonComponent } from "@shared/component/language-button/language-button.component";
+import { TranslateModule } from '@ngx-translate/core'; 
+import { TopBarComponent } from "@shared/component/top-header/top-bar.component";  
 import { ResponsiveService } from '@shared/services/responsive.service';
 import { MatIcon } from "@angular/material/icon";
 
 @Component({
     selector: 'app-admin-header-section',
-    imports: [CommonModule, TranslateModule, TopBarComponent, LoginLogoutButtonComponent, LanguageButtonComponent, MatIcon],
+    imports: [CommonModule, TranslateModule, TopBarComponent, MatIcon],
     templateUrl: './admin-header-section.component.html',
     styleUrl: './admin-header-section.component.scss'
 })
