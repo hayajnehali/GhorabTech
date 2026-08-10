8946544b1908748a2d4fd451b04943e8622ebe6a
Ali Mohammad Hayajneh <hayajneh030@gmail.com>
2026-08-10 04:59:10 +0300

    Updates

---
+++ chunk-C6M4NY5K.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-YDUMNWPZ.js";import{Fa as t,Ha as r}from"./chunk-6LPWGCIK.js";var u=class extends t{cartItemId;quantity=1;product=new o;productVariantId="";productVariant;get total(){return this.quantity*(this.productVariant?.price??0)}constructor(){super()}};var n=class extends r{cartId};export{u as a,n as b};
